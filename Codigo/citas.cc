69e28288c7a310777de407ec4a675a59d77f7bb4
i82egjur <55484111+i82egjur@users.noreply.github.com>
2019-12-19 18:27:14 +0100

    citas.cc definitivo

--- Codigo/citas.cc
+++ Codigo/citas.cc
@@ -14,7 +14,8 @@ void Citas :: menu_citas()
 	cout<<"* 0. Salir          *"<<endl;
 	cout<<"* 1. Añadir cita    *"<<endl;
 	cout<<"* 2. Consultar Cita *"<<endl;
-	cout<<"* 3. Modificar cita  *"<<endl;
+	cout<<"* 3. Eliminar cita  *"<<endl;
+	cout<<"* 4. Modificar cita *"<<endl;
 	cout<<"*********************"<<endl;
 	cout<<"Eleccion >>";
 	cin>>ele;
@@ -31,6 +32,10 @@ void Citas :: menu_citas()
 		break;
 		case 3:
 		system("clear");
+		eliminar();
+		break;
+		case 4:
+		system("clear");
 		modificar_Cita();
 		break;
 		case 0:
@@ -39,12 +44,8 @@ void Citas :: menu_citas()
 		break;
 	}
 }
-
-int Citas::modificar_Cita()
+void Citas::modificar_Cita()
 {
-	cout<<"Introduzca 1, 2, 3, según corresponda"<<endl;
-	cout<<"1. Borrar cita reservada"<<endl;
-	cout<<"2. Cambiar de fecha y hora"<<endl;
 	string dia, mes, anio;
 	cout<<"Introduzca el dia de la cita que quiere modificar"<<endl;
 	cin>>dia;
@@ -52,29 +53,171 @@ int Citas::modificar_Cita()
 	cin>>mes;
 	cout<<"Introduzca el año"<<endl;
 	cin>>anio;
-	/*
+	eliminarCita(dia, mes, anio);
+	cout<<"Introduzca los nuevos datos de la cita"<<endl;
+	aniadir_cita();
+	reservaCitas();
+
+
+}
+void Citas::imprimirCitasReservadas(string dia, string mes, string anio)
+{
 	struct datos_lista_citas da;
 	lista_.clear();
-	ifstream leer1(getDia_reserva() +"-"+ getMes_reserva()+"-" + getAnio_reserva()+"-reservado");
+	ifstream leer1(dia +"-"+ mes+"-" +anio+"-reservado");
 	if(!leer1)
 	{
-		cout<<"Error al abrir"<<endl;
-		return false;
+		cout<<"ERROR: No hay ninguna fecha reservada ese dia"<<endl;
 	} 	
-	
-	while(leer1.getline(hora,10, ','))
+	else
 	{
-		leer1.getline(minus, 10,':');
-		leer1.getline(minus, 50,'\n');
-    	da.hora=atoi(hora);
-    	da.minuto=atoi(minus);
-    	da.nombre=
-  		lista_.push_back(da);
+		char hor[10], minus[10];
+		char nom[50];
+		while(leer1.getline(hor,10, ','))
+		{
+			leer1.getline(minus, 10,':');
+			leer1.getline(nom, 50,'\n');
+	    	da.hora=atoi(hor);
+	    	da.minuto=atoi(minus);
+	    	da.nombre=nom;	
+	  		lista_.push_back(da);
+		}
+		list<struct datos_lista_citas>::iterator i;
+		for(i=lista_.begin(); i!=lista_.end(); i++)
+		{
+			cout<<(*i).hora<<":"<<(*i).minuto<<"<-Reservada por: "<<(*i).nombre<<"\n";
+		}
+	}
+	leer1.close();
+}
+void Citas::eliminar()
+{
+	string dia, mes, anio;
+	
+	cout<<"Introduzca el dia de la cita"<<endl;
+	cin>>dia;
+	cout<<"Introduzca el mes de la cita"<<endl;
+	cin>>mes;
+	cout<<"Introduzca el año"<<endl;
+	cin>>anio;
+	eliminarCita(dia, mes, anio);	
+}
+
+void Citas::eliminarCita(string dia, string mes, string anio)
+{
+
+	char hor[10], minus[10];
+	char nom[50];
+	struct datos_lista_citas da;
+	lista_.clear();
+	fstream leer1(dia +"-"+ mes+"-" +anio+"-reservado");
+	if(!leer1)
+	{
+		cout<<"ERROR: No hay ninguna fecha reservada ese dia"<<endl;
+	} 	
+	else
+	{
+		while(leer1.getline(hor,10, ','))
+		{
+			leer1.getline(minus, 10,':');
+			leer1.getline(nom, 50,'\n');
+	    	da.hora=atoi(hor);
+	    	da.minuto=atoi(minus);
+	    	da.nombre=nom;	
+	  		lista_.push_back(da);
+		}
+		imprimirCitasReservadas(dia, mes, anio);
+		cout<<"Inserte la hora"<<endl;
+		cout<<"hora -> ";
+		cin>>hor;
+		cout<<"minuto -> ";
+		cin>>minus;
+		list<struct datos_lista_citas>::iterator i;
+		for(i=lista_.begin(); i!=lista_.end(); i++)
+		{
+			if((*i).hora==atoi(hor) && (*i).minuto==atoi(minus))
+			{
+				i=lista_.erase(i);
+			}
+		}	
+		leer1.close();
+		ofstream sobreescribir(dia +"-"+ mes+"-" +anio+"-reservado");
+		if(!sobreescribir)
+		{
+			cout<<"Error"<<endl;
+		} 	
+		for(i=lista_.begin(); i!=lista_.end(); i++)
+		{
+			leer1 << (*i).hora << "," << (*i).minuto<<":"<<apellido1<<" "<<apellido2<<", "<<nombre<<"\n";	
+		}	
+		sobreescribir.close();
+		lista_.clear();
+		ifstream leer1(dia +"-"+ mes+"-" + anio+"-libres");
+		if(!leer1)
+		{
+			cout<<"Error"<<endl;
+		} 	
+		char h[10], m[10];
+		while(leer1.getline(h,10, ','))
+		{
+			leer1.getline(m, 10,'\n');
+			if(atoi(minus)==0)
+			{
+				if(atoi(h)==atoi(hor)-1 && atoi(m)==45)
+				{
+				
+					da.hora=atoi(h);
+					da.minuto=atoi(m);
+					lista_.push_back(da);
+					da.hora=atoi(hor);
+					da.minuto=atoi(minus);
+					lista_.push_back(da);
+				}
+				else
+				{
+					da.hora=atoi(h);
+					da.minuto=atoi(m);
+					lista_.push_back(da);
+				}
+			}
+			else
+			{
+
+				if(atoi(h)==atoi(hor) && atoi(m)==atoi(minus)-15)
+				{
+					da.hora=atoi(h);
+					da.minuto=atoi(m);
+					lista_.push_back(da);
+					da.hora=atoi(hor);
+					da.minuto=atoi(minus);
+					lista_.push_back(da);
+					
+				}
+				else
+				{
+					da.hora=atoi(h);
+					da.minuto=atoi(m);
+					lista_.push_back(da);
+				}
+			}
+		}
+		ofstream sobreescribirL(dia +"-"+ mes+"-" +anio+"-libres");
+		if(!sobreescribirL)
+		{
+			cout<<"Error"<<endl;
+		} 	
+		for(i=lista_.begin(); i!=lista_.end(); i++)
+		{
+			sobreescribirL<< (*i).hora << "," << (*i).minuto<<"\n";	
+		}	
+		sobreescribirL.close();
+		char n;
+		cout<<"Pulse una tecla para continuar"<<endl;
+		cin>>n;
 	}
 
-*/
-	  
 }
+
 bool Citas :: confirmafecha(string dr, string mr)
 {
 	char dia[dr.length()+1], mes[mr.length()+1], anio[anio_reserva_.length()+1];
@@ -84,8 +227,11 @@ bool Citas :: confirmafecha(string dr, string mr)
 	int m=atoi(mes);
 	int d=atoi(dia);
 	int a=atoi(anio);
-
-
+	if(stoi(anio_reserva_)<anio_sistema)
+	{
+		cout<<"Introduzca una fecha válida"<<endl;
+		return false;
+	}
 	if(m == 1 || m== 3|| m== 5|| m==7 || m==8 || m==10 || m==12)
 	{
 		if(d>=1 && d <=31)
@@ -134,9 +280,8 @@ bool Citas :: confirmafecha(string dr, string mr)
 void Citas ::aniadir_cita()
 {
 
-	string dr, mr;
+	string dr, mr, an;
 	int confirmacion;
-	char si_no[2];
 	do
 	{
 		cout<<"******************"<<endl;
@@ -149,7 +294,13 @@ void Citas ::aniadir_cita()
 		cout<<"******************"<<endl;
 		cin>>mr;
 		system("clear");
-		cout<<"¿La fecha seleccionada es correcta? -> "<<dr<<" / "<<mr<<endl;
+		cout<<"******************"<<endl;
+		cout<<"* Introduzca año *"<<endl;
+		cout<<"******************"<<endl;
+		cin>>an;
+		setAnio_reserva(an);
+		system("clear");
+		cout<<"¿La fecha seleccionada es correcta? -> "<<dr<<" / "<<mr<<"/"<<an<<endl;
 		cout<<"1 -> SI || 2-> NO"<<endl;
 		cin>>confirmacion;
 	}
@@ -160,7 +311,6 @@ void Citas ::aniadir_cita()
 	strcpy(mes, mr.c_str());
 	setDia_reserva(dia);
 	setMes_reserva(mes);
-	setAnio_reserva(atoi(dia), atoi(mes));
 	imprimirCitasLibres();
 
 	
@@ -168,27 +318,9 @@ void Citas ::aniadir_cita()
 
 
 
-void Citas ::  setAnio_reserva(int dr, int mr)
+void Citas ::  setAnio_reserva(string anio)
 {
-	int anio_reserva;
-	if(mr < mes_sistema)
-	{
-		anio_reserva=anio_sistema+1;
-	}
-	if(mr==mes_sistema)
-	{
-		if(dr<dia_sistema)
-		{
-			anio_reserva=anio_sistema+1;
-		}
-	}
-	else
-	{	
-		anio_reserva=anio_sistema;
-	}	
-	char aux[20];
-	sprintf(aux, "%d", anio_reserva);
-	anio_reserva_=aux;
+	anio_reserva_=anio;
 }
 
 
@@ -202,9 +334,7 @@ Citas::Citas()
  
 	 anio_sistema=tiempo->tm_year + 1900;
 	 mes_sistema=tiempo->tm_mon + 1;
-	 dia_sistema=tiempo->tm_mday;
-
-	
+	 dia_sistema=tiempo->tm_mday;	
 	
 }
 
@@ -215,7 +345,7 @@ void Citas :: crear_fichero_cl()
 	if(!salida) cout << "ERROR de apertura";
 	for(int hor=9; hor<20; hor++)
 	{
-		for(int m=0; m<3; m++)
+		for(int m=0; m<4; m++)
 		{
 			min=m*15;
 			salida << hor << "," << min<<"\n";
@@ -243,7 +373,7 @@ bool Citas :: imprimirCitasLibres()
 	while(leer1.getline(hora,10, ','))
 	{
 		leer1.getline(minus, 10,'\n');
-    	da.hora=atoi(hora);
+	 	da.hora=atoi(hora);
     	da.minuto=atoi(minus);
   		lista_.push_back(da);
 	}
@@ -284,7 +414,8 @@ bool Citas :: imprimirCitas(string dr, string mr, string anio)
 	list<struct datos_lista_citas>::iterator i;	
 	for(i=lista_.begin(); i!=lista_.end(); i++)
 	{
-		cout<<(*i).hora<<":"<<(*i).minuto<<"\n";			
+		cout<<(*i).hora<<":"<<(*i).minuto<<"\n";
+			
 	}
 	leer1.close();
 	cout<<"Pulse una tecla para mostrar las citas reservadas"<<endl;
@@ -320,7 +451,6 @@ bool Citas :: imprimirCitas(string dr, string mr, string anio)
 	cin>>n;
 	return true;
 	
-	
 		
 }
 
@@ -339,13 +469,25 @@ bool Citas :: imprimirCitas()
 	cout<<"Citas libres"<<endl;
 	char hora[10], minus[10];
 	
-	struct datos_lista_citas da;
-	
+	struct datos_lista_citas da;	
+
 	ifstream leer1(dr +"-"+ mr+"-" + anio+"-libres");
 	if(!leer1)
 	{
-		cout<<"error al abrir"<<endl;
-		return false;
+		int min;
+		ofstream salida(dr +"-"+ mr+"-" + anio+"-libres");
+		if(!salida) cout << "ERROR de apertura";
+		for(int hor=9; hor<20; hor++)
+		{
+			for(int m=0; m<4; m++)
+			{
+				min=m*15;
+				salida << hor << "," << min<<"\n";
+			}
+		}	
+		salida.close();
+		imprimirCitas();
+
 	} 	
 	char n;
 	lista_.clear();
@@ -356,21 +498,12 @@ bool Citas :: imprimirCitas()
     	da.minuto=atoi(minus);
   		lista_.push_back(da);
 	}
-	int contador=1;
 	list<struct datos_lista_citas>::iterator i;	
 	for(i=lista_.begin(); i!=lista_.end(); i++)
 	{
-		cout<<"* "<<(*i).hora<<":"<<(*i).minuto<<" *"<<" ";
-		if(contador==3)
-		{
-			cout<<endl;
-			contador=0;
-
-		}
-		contador++;
-		
+		cout<<(*i).hora<<":"<<(*i).minuto<<"\n";
+			
 	}
-	cout<<endl;
 	leer1.close();
 	cout<<"Pulse una tecla para mostrar las citas reservadas"<<endl;
 	cin>>n;
@@ -380,7 +513,7 @@ bool Citas :: imprimirCitas()
 	ifstream leer2(dr +"-"+ mr+"-" + anio+"-reservado");
 	if(!leer2)
 	{
-		cout<<"error al abrir"<<endl;
+		cout<<"No hay citas reservadas para hoy"<<endl;
 		return false;
 	} 	
 	
@@ -393,59 +526,100 @@ bool Citas :: imprimirCitas()
     	da.nombre=nombre;
   		lista_.push_back(da);
 	}
-	cout<<"Citas reservadas \n"<<endl;
+
 	for(i=lista_.begin(); i!=lista_.end(); i++)
 	{
-		cout<<"* "<<(*i).hora<<":"<<(*i).minuto<<"<-Reservada por: "<<(*i).nombre<<"\n";
+		cout<<(*i).hora<<":"<<(*i).minuto<<"<-Reservada por: "<<(*i).nombre<<"\n";
 			
 	}
 	leer2.close();
-	cout<<"Pulse una tecla para continuar"<<endl;
-	cin>>n;
 	return true;
 	
 		
 }
+bool Citas::existe()
+{
+	char nom[50], apellido_1[50], apellido_2[50];
+	ifstream exist("Pacientes.txt");
+	if(!exist)
+	{
+		cout<<"No hay ningun paciente registrado, añada un paciente primero"<<endl;
+		return false;
+	}
+	while(exist.getline(nom, 50, '_'))
+	{
+		exist.getline(apellido_1, 50, '_');
+		exist.getline(apellido_2, 50, '\n');
+		if(nom==nombre && apellido_1==apellido1 && apellido_2==apellido2)
+		{
+			return true;
+		}
+	}
+	cout<<"No esta"<<endl;
+	return false;
+}
 void Citas :: reservaCitas()
 {	
-	cout<<"Introduca hora"<<endl;
-	cin>>hora_reserva;
+	int hora, minute;
+	cout<<"Introduzca hora"<<endl;
+	cin>>hora;
 	cout<<"minuto"<<endl;
-	cin>>minuto_reserva;
-	getchar();
-	cout<<"Apellidos del paciente \n";
-	getline(cin, apellidos);
-	cout<<"Nombre del paciente \n";
-	getline(cin, nombre);
-	list<struct datos_lista_citas>::iterator i;	
-	for(i=lista_.begin(); i!=lista_.end(); i++)
+	cin>>minute;
+	if(hora<9 || hora > 19)
 	{
-		if((*i).hora==hora_reserva && (*i).minuto==minuto_reserva)
+		cout<<"ERROR"<<endl;
+	}
+	if(minute%15!=0)
+	{
+		cout<<"ERROR"<<endl;
+	}
+	else
+	{
+		hora_reserva=hora;
+		minuto_reserva=minute;
+		getchar();
+		cout<<"Introduzca el primer apellido del paciente \n";
+		getline(cin, apellido1);
+		cout<<"Introduzca el segundo apellido del paciente \n";
+		getline(cin, apellido2);
+		cout<<"Nombre del paciente \n";
+		getline(cin, nombre);
+		string apellidos=(apellido1 + " "+apellido2);
+		if(existe()==true)
 		{
-			cout<<"entra \n";
-			fstream fichero_cReservadas;
-			fichero_cReservadas.open(getDia_reserva() +"-"+ getMes_reserva()+"-" + getAnio_reserva()+"-reservado", ios::app);
-			if(!fichero_cReservadas.is_open()) cout << "ERROR de apertura";			
-			fichero_cReservadas << (*i).hora << "," << (*i).minuto<<":"<<apellidos<<", "<<nombre<<"\n";	
-			i=lista_.erase(i);
-			fichero_cReservadas.close();
+		
+			list<struct datos_lista_citas>::iterator i;	
+			for(i=lista_.begin(); i!=lista_.end(); i++)
+			{
+				if((*i).hora==hora_reserva && (*i).minuto==minuto_reserva)
+				{
+					cout<<"entra \n";
+					fstream fichero_cReservadas;
+					fichero_cReservadas.open(getDia_reserva() +"-"+ getMes_reserva()+"-" + getAnio_reserva()+"-reservado", ios::app);
+					if(!fichero_cReservadas.is_open()) cout << "ERROR de apertura";			
+					fichero_cReservadas << (*i).hora << "," << (*i).minuto<<":"<<apellidos<<", "<<nombre<<"\n";	
+					i=lista_.erase(i);
+					fichero_cReservadas.close();
+				}
+
+			} 
+			
+			for(i=lista_.begin(); i!=lista_.end(); i++)
+			{
+				cout<<(*i).hora<<":"<<(*i).minuto<<endl;
+			}
+
+			ofstream fichero_cLibres(getDia_reserva() +"-"+ getMes_reserva()+"-" + getAnio_reserva()+"-libres");
+			if(!fichero_cLibres) cout << "ERROR de apertura";
+
+			for(i=lista_.begin(); i!=lista_.end(); i++)
+			{
+				fichero_cLibres << (*i).hora << "," << (*i).minuto<<"\n";
+			}
+			fichero_cLibres.close();
+		
 		}
-
-	} 
-	
-	for(i=lista_.begin(); i!=lista_.end(); i++)
-	{
-		cout<<(*i).hora<<":"<<(*i).minuto<<endl;
 	}
-
-	ofstream fichero_cLibres(getDia_reserva() +"-"+ getMes_reserva()+"-" + getAnio_reserva()+"-libres");
-	if(!fichero_cLibres) cout << "ERROR de apertura";
-
-	for(i=lista_.begin(); i!=lista_.end(); i++)
-	{
-		fichero_cLibres << (*i).hora << "," << (*i).minuto<<"\n";
-	}
-	fichero_cLibres.close();
 }
 
 void Citas:: consultarCitas()
@@ -463,13 +637,3 @@ void Citas:: consultarCitas()
 	cout<<"Fecha seleccionada"<<endl;
 	imprimirCitas(dia, mes, anio);
 }
-
-/*
-
-int main()
-{
-	system("clear");
-	Citas c;
-	c.imprimirCitas();
-	c.menu_citas();	
-}*/
